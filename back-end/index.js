d242ac4650cd3a0b718f316c021ddef457f0a0f5
Jamil-Hassan86 <jamil.hassan8568@gmail.com>
2024-03-01 16:53:27 +0000

    Setting localhost

--- back-end/index.js
+++ back-end/index.js
@@ -2,3 +2,7 @@ const express = require ("express");
 const mysql = require("mysql2");
 
 const app = express();
+
+app.listen(8000, () => {
+    console.log("Server is on localhost port 8000");
+});
