e864816a59deb8ae5c823b5e0b828d05ab914e4d
Jamil-Hassan86 <jamil.hassan8568@gmail.com>
2024-03-04 14:42:39 +0000

    Creating a sign up page

--- back-end/index.js
+++ back-end/index.js
@@ -2,7 +2,7 @@ const express = require ("express");
 const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const port = 8000;
+const port = 3000;
 
 const app = express();
 
---
+++ front-end/js/form.js
@@ -0,0 +1,33 @@
+const signupForm = document.getElementById("signup-form");
+
+        signupForm.addEventListener("submit", async (event) => {
+            event.preventDefault();
+
+            const name = document.getElementById("name").value;
+            const email = document.getElementById("email").value;
+            const password = document.getElementById("password").value;
+            const fitness_plan = document.getElementById("fitness_plan").value;
+
+            try {
+                const response = await fetch("http://localhost:3000/api/user/create", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        name,
+                        email,
+                        password,
+                        fitness_plan
+                    })
+                });
+
+                if (!response.ok) {
+                    throw new Error("Error creating user account.");
+                }
+
+                console.log("User account created.");
+            } catch (error) {
+                console.error(error);
+            }
+        });
