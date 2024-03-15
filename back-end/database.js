1424384d5bcf5761fc5fdaa0ed65f5537dfab2a5
Jamil-Hassan86 <jamil.hassan8568@gmail.com>
2024-03-15 16:33:14 +0000

    changed password of database

--- back-end/database.js
+++ back-end/database.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Corrosive123",
+    password: "123456789",
     database: "fitness_db"
 });
 
